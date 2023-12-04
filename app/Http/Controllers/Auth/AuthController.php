da4a31c9d20147c7577adc7862474988930271bd
Putri Pratiwi <ppratiwi113@gmail.com>
2023-12-04 15:24:33 +0700

    Mengubah data pada Model, Controller, dan Route

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -35,77 +35,9 @@ class AuthController extends Controller
         $secretKey = env('JWT_SECRET'); // Ganti dengan secret key yang sebenarnya
         $payload = [
             'username' => $username,
-            'exp' => time() + (60 * 60),
+            'exp' => time() + (30 * 24 * 60 * 60) // 30 hari * 24 jam * 60 menit * 60 detik (satu bulan)
             // ...Tambahkan data tambahan yang ingin dimasukkan ke dalam token JWT
         ];
         return JWT::encode($payload, $secretKey, 'HS256');
     }
 }
-
-// class AuthController extends Controller
-// {
-    // protected $response;
-    // public function __construct()
-    // {
-    //     $this->middleware('guest', ['except' => 'getLogout']);
-    //     // $this->response = new Response(); // Gunakan class Response
-    // }
-
-    // public function login(Request $request)
-    // {
-    //     $credentials = $request->only('username','password');
-
-    //     if(!Auth::authenticate($credentials)) {
-    //         return response()->json(['error'=>'Unauthorized', 401]);
-    //     }
-
-    //     $token = JWTAuth::fromUser($request->username);
-    //     return response()->json(compact('token'));
-
-        // try{
-        //     if(!$token=JWTAuth::attempt($credentials)) {
-        //         return response()->json(['error'=>'invalid_credentials', 401]);
-        //     }
-        // } catch(JWTException $e) {
-        //     //Something went wrong!
-        //     return response()->json(['error'=>'could_not_create_token'], 500);
-        // }
-
-        
-
-        //// if no errors are encountered we can return a JWT
-        
-    // 
-
-    // public function getAuthenticatedUser() {
-    //     try {
-    //         if(!$user = JWTAuth::parseToken()->authenticate()) {
-    //             return response()->json(['user_not_found'], 404);
-    //         }
-    //     }catch (TokenExpiredException $e) {
-    //         return response()->json(['token_expired'], $e->getStatusCode());
-    //     }catch (TokenInvalidException $e) {
-    //         return response()->json(['token_invalid'], $e->getStatusCode());
-    //     }catch (JWTException $e) {
-    //         return response()->json(['token_absent'], $e->getStatusCode());
-    //     }
-
-    //     return response()->json(compact('user'));
-    // }
-
-    // public function register(Request $request)
-    // {
-    //     $newuser = $request->all();
-    //     $password = Hash::make($request->input('password'));
-    //     $newuser['password'] = $password;
-    //     return User::create($newuser);
-    // }
-
-    // protected function create(array $data)
-    // {
-    //     return User::create([
-    //         'name' => $data['name'],
-    //         'email' => $data['email'],
-    //         'password' => bcrypt($data['password']),
-    //     ]);
-    // }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,27 +1,6 @@
 <?php
 
-
-// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// use Tymon\JWTAuth\Facades\JWTAuth;
-// use App\Http\Controllers\Sdid\API\JumlahDosenController;
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/api/jumlah-dosen', 'API\\ApiController@getJumlahDosen');
 
 
 //login token statis
@@ -40,7 +19,7 @@ Route::group(['middleware' => 'jwt-auth'], function(){
     Route::get('/api/bentukpenddosen', 'API\\ApiController@getBentukPendDosen');
     Route::get('/api/bentukpendtendik', 'API\\ApiController@getBentukPendTendik');
     Route::get('/api/jmlhpangkatdosen', 'API\\ApiController@getJumPangkatDosen');
-    Route::get('/api/jenjangpenddosen', 'API\\ApiController@getJenjPendDosen');
+    // Route::get('/api/jenjangpendtendik', 'API\\ApiController@getJenjPend');
     Route::get('/api/jmlhpangkattendik', 'API\\ApiController@getJumPangkatTendik');
     Route::get('/api/jmlhpngktdosenahli', 'API\\ApiController@getJumPangDosenperAhli');
     Route::get('/api/jumptaktifprov', 'API\\ApiController@getJumPTAktifPerProv');
@@ -53,10 +32,3 @@ Route::group(['middleware' => 'jwt-auth'], function(){
     Route::get('/api/trendusulanserdos', 'API\\ApiController@getTrendUsulanSerdos');//masih error
     Route::get('/api/trendsertdosen', 'API\\ApiController@getTrendSertDosen');
 });
-
-
-
-
-
-// Route::middleware(['csrf.api'])->post('/api/register', 'Auth\\AuthController@register');
-// Route::middleware(['csrf.api'])->post('/api/authenticate', 'Auth\AuthController@authenticate');
